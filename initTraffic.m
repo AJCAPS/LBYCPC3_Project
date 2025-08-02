5103927a0467d5a71f70789b707c3fba31bd25fc
AJCAPS <aaron_james_capinpin@dlsu.edu.ph>
2025-08-02 12:12:56 +0800

    Function called to initialize Variables

---
+++ initTraffic.m
@@ -0,0 +1,10 @@
+% initTraffic.m CALL THIS FUNCTION TO INITIATE THE VALUES IN THE SLX
+T           = 600;
+dt          = 1;
+lambda      = [0.5; 0.7; 0.6; 0.8];
+trafficData = genTrafficData(T, dt, lambda);
+% Create timeseries objects for intersection #1
+tsArr1   = timeseries(trafficData.noisyArrival(1,:)', trafficData.time');
+tsDelay1 = timeseries(trafficData.delay(1,:)',        trafficData.time');
+L_road      = 0.5;
+serviceRate = 2;
