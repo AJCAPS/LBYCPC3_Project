2cfe304429b74f524adf62b73a1a5053258726be
AJCAPS <aaron_james_capinpin@dlsu.edu.ph>
2025-08-02 12:09:43 +0800

    Updated Simulink and Adjusted Traffic_Controller to accomodate changes

--- genTrafficData.m
+++ genTrafficData.m
@@ -1,9 +1,11 @@
 function data = genTrafficData(T, dt, lambda)
     steps = round(T/dt);
     mu    = lambda(:) * dt;            % Lambda = vehicles
-    M     = repmat(mu, 1, steps);      
+    M     = repmat(mu, 1, steps);
+    serviceRate = 2;   % vehicles per second
     data.arrival      = poissrnd(M);   
     data.noisyArrival = data.arrival + round(3*randn(size(M)));
     data.delay        = exprnd(0.2, size(M));
     data.time         = (0:steps-1)*dt;
+    data.ArrivalCount = data.noisyArrival;
 end
